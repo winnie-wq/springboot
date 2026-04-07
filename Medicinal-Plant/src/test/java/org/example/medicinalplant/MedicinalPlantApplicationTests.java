dada579fc159c5478f72c8d0be893cf0cdc25298
winnie-wq <hlow3143@gmail.com>
2026-04-08 00:15:47 +0800

    整合LanChain4J框架

--- Medicinal-Plant/src/test/java/org/example/medicinalplant/MedicinalPlantApplicationTests.java
+++ Medicinal-Plant/src/test/java/org/example/medicinalplant/MedicinalPlantApplicationTests.java
@@ -1,5 +1,6 @@
 package org.example.medicinalplant;
 
+import dev.langchain4j.model.openai.OpenAiChatModel;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,13 @@ class MedicinalPlantApplicationTests {
 
     @Test
     void contextLoads() {
+        OpenAiChatModel aiChatModel = OpenAiChatModel.builder()
+                .modelName("gpt-4o-mini")
+                .apiKey("demo")
+                .build();
+
+        String chat = aiChatModel.chat("你好，你是谁呀");
+        System.out.println(chat);
     }
 
 }
