6320cfe115dad594adfe9acc2f31efe2c941ec51
winnie-wq <hlow3143@gmail.com>
2026-04-09 22:17:28 +0800

    前后端分离

--- Medicinal-Plant/src/main/java/org/example/medicinalplant/controller/AiController.java
+++ Medicinal-Plant/src/main/java/org/example/medicinalplant/controller/AiController.java
@@ -5,11 +5,13 @@ import org.example.medicinalplant.config.AiConfig;
 import dev.langchain4j.model.chat.ChatLanguageModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/ai")
 public class AiController {
